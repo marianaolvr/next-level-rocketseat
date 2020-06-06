92e718edc7bbb399bf02fd619f67dea8d3fda2dc
Mariana Oliveira <olvr.mariana@gmail.com>
2020-06-06 00:33:54 -0300

    Creates the functionality of listing items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World' });
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+    // SELECT * FROM items
 
+    const serializedItems = items.map( item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }; 
+    })
+
+    return response.json(serializedItems);
 });
 
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
@@ -6,4 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use('/uploads', express.static(path.resolve(__dirname, '.', 'uploads')));
+// express.static = servir arquivos estáticos de uma pasta específica
+
+
+
 app.listen(3333);
+
